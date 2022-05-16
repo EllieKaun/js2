36c004b7d3eab23f3c940f14f223e245453b45c8
IlyaFrontJs <ilyafazylbaev@mail.ru>
2022-05-16 15:50:06 +0600

    initial commit

---
+++ lesson3/food/js/app.js
@@ -0,0 +1,65 @@
+const tabs = document.querySelectorAll(".tabheader__item");
+const tabsParent = document.querySelector(".tabheader__items");
+const tabContent = document.querySelectorAll(".tabcontent");
+
+const hideTabContent = () => {
+  tabContent.forEach((item) => {
+    item.style.display = "none";
+  });
+  tabs.forEach((item) => {
+    item.classList.remove("tabheader__item_active");
+  });
+};
+
+const showTabContent = (i = 0) => {
+  tabContent[i].style.display = "block";
+  tabs[i].classList.add("tabheader__item_active");
+};
+
+hideTabContent();
+showTabContent();
+
+tabsParent.addEventListener("click", (event) => {
+  const target = event.target;
+
+  if (target.classList.contains("tabheader__item")) {
+    tabs.forEach((item, i) => {
+      if (target === item) {
+        console.log(i);
+        hideTabContent();
+        showTabContent(i);
+      }
+    });
+  }
+});
+
+const modal = document.querySelector(".modal");
+const modalTrigger = document.querySelector(".btn_white");
+const closeModalBtn = document.querySelector(".modal__close");
+
+console.log(modal, "modal");
+console.log(modalTrigger, " modalTrigger");
+console.log(closeModalBtn, " closeModalBtn");
+
+const openModal = () => {
+  modal.classList.add("show");
+  modal.classList.remove("hide");
+  document.body.style.overflow = "hidden";
+};
+
+const closeModal = () => {
+  modal.classList.add("hide");
+  modal.classList.remove("show");
+  document.body.style.overflow = "";
+};
+
+modalTrigger.addEventListener("click", openModal);
+
+modal.addEventListener("click", (event) => {
+  if (event.target == modal) {
+    closeModal();
+    console.log(event.target);
+  }
+});
+
+closeModalBtn.addEventListener("click", closeModal);
